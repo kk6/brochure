a19197c9189a2255e6d1eda3f204630e9cbb5e1d
kk6 <hiro.ashiya@gmail.com>
2017-12-30 21:03:42 +0900

    TemplateView with index.html

--- brochure/brochure/settings.py
+++ brochure/brochure/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'brochure.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -118,3 +118,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+TWITTER_CALLBACK_URL = 'http://127.0.0.1:8000/callback/'
