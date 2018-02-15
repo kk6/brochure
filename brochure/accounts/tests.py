0d1104b45b4434b8b2d8602c7949d2cc0a8d0ef7
kk6 <hiro.ashiya@gmail.com>
2018-02-15 23:10:39 +0900

    Rewrite tests using pytest

--- brochure/accounts/tests.py
+++ brochure/accounts/tests.py
@@ -1,26 +1,49 @@
-from django.test import TestCase, RequestFactory
-from brochure.middleware import TwitterMiddleware
-from .views import dashboard
+# -*- coding: utf-8 -*-
+import pytest
 
 
-class TwitterMiddleWareTest(TestCase):
-    def setUp(self):
-        self.middleware = TwitterMiddleware(get_response=None)
-        self.factory = RequestFactory()
+def _create_request(path, session, user='dummy'):
+    from django.test import RequestFactory
+    request = RequestFactory().get(path)
+    request.session = session
+    request.user = user
+    return request
 
-    def test_if_session_has_not_access_token_then_redirect_top(self):
-        request = self.factory.get('/dashboard/')
-        request.session = {}
-        request.user = 'user'
-        response = self.middleware.process_view(request, dashboard, (), {})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response.url, '/')
 
-    def test_if_session_has_access_token_then_request_has_twitter_api(self):
-        request = self.factory.get('/dashboard/')
-        request.session = {'access_token_data': {'access_token': 'dummy_access_token',
-                                                 'access_token_secret': 'dummy_access_token_secret'}}
-        request.user = 'user'
-        response = self.middleware.process_view(request, dashboard, (), {})
-        self.assertIsNone(response)
-        self.assertTrue(hasattr(request, 'twitter_api'))
+@pytest.fixture
+def dashboard_request_with_no_session():
+    return _create_request('/dashboard/', {})
+
+
+@pytest.fixture
+def dashboard_request_with_session():
+    session = {'access_token_data': {'access_token': 'dummy_access_token',
+                                             'access_token_secret': 'dummy_access_token_secret'}}
+    return _create_request('/dashboard/', session)
+
+
+@pytest.fixture
+def dashboard_view():
+    from .views import dashboard
+    return dashboard
+
+
+class TestTwitterMiddleWare:
+
+    def _makeOne(self):
+        from brochure.middleware import TwitterMiddleware
+        return TwitterMiddleware(get_response=None)
+
+    def test_if_session_has_not_access_token_then_redirect_top(self, dashboard_request_with_no_session, dashboard_view):
+        middleware = self._makeOne()
+        response = middleware.process_view(dashboard_request_with_no_session, dashboard_view, (), {})
+        assert response.status_code == 302
+        assert response.url == '/'
+
+    def test_if_session_has_access_token_then_request_has_twitter_api(self,
+                                                                      dashboard_request_with_session,
+                                                                      dashboard_view):
+        middleware = self._makeOne()
+        response = middleware.process_view(dashboard_request_with_session, dashboard_view, (), {})
+        assert response is None
+        assert hasattr(dashboard_request_with_session, 'twitter_api')
---
+++ brochure/brochure/pytest_settings.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+from .settings import *
+
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': ':memory:',
+}
